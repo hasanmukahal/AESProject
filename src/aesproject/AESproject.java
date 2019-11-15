ce02850e5ad64ce8a8ef41f246c85a697f888a74
hasanmukahal <hasan000215@hotmail.com>
2019-11-16 00:37:24 +0200

    Add files via upload

--- src/aesproject/AESproject.java
+++ src/aesproject/AESproject.java
@@ -5,6 +5,9 @@
  */
 package aesproject;
 
+import static aesproject.Polynomial.Add;
+import java.util.Arrays;
+
 
 /**
  *
@@ -19,14 +22,232 @@ public class AESproject {
         // TODO code application logic here
         
         
-        String b="x^7+x+1";
-        String m="x^8+x^4+x^3+x+1";
-        	Polynomial A1 = new Polynomial(0, 0);
-		Polynomial A2 = Polynomial.Add(m);
-		Polynomial B1 = new Polynomial(0, 1);
-		Polynomial B2 = Polynomial.Add(b);
-        System.out.println(Polynomial.findInverse(b,m,A1,A2,B1,B2));
-        
+       
+       // System.out.println(Polynomial.findInverse(b,m,A1,A2,B1,B2));
+       // System.out.println("hasan");
+       // System.out.println(hexa1bytetoBinary('0', 'F'));
+              //  System.out.println(BinaryToHexa(hexa1bytetoBinary('0', 'F')));
+             //  System.out.println(BinarytoPoly(hexa1bytetoBinary('0', 'F'))); 
+            //  System.out.println(polytoBinary(b));
+       // System.out.println(SubstituteByteTransformation(Polynomial.findInverse(b,m,A1,A2,B1,B2)));
+    //    System.out.println(hexa1bytetoBinary('8', 'A'));
+       // System.out.println(getSubbytefrombinary(hexa1bytetoBinary('8', 'A')));
+        System.out.println(SubstituteByteTransformation('9','5'));
     }
+   
+    public static String findInverse(String b){
+            String m="x^8+x^4+x^3+x+1";
+
+    Polynomial A1 = new Polynomial(-1);
+		Polynomial A2 = Add(m);
+		Polynomial B1 = new Polynomial(0);
+		Polynomial B2 = Add(b);
+               
+return Polynomial.findpolyInverse(A1,A2,B1,B2).toString();
     
+    
+    
+    }
+    public static String hexatoBinary(char a){
+    
+  
+        switch (a) { 
+        case '0': 
+           return "0000"; 
+         
+        case '1': 
+           return "0001"; 
+           
+        case '2': 
+           return "0010"; 
+          
+        case '3': 
+          return "0011"; 
+          
+        case '4': 
+           return  "0100"; 
+        
+        case '5': 
+          return "0101"; 
+         
+        case '6': 
+           return "0110"; 
+            
+        case '7': 
+          return "0111"; 
+           
+        case '8': 
+          return  "1000"; 
+            
+        case '9': 
+        return "1001"; 
+          
+        case 'A': 
+        case 'a': 
+           return "1010"; 
+           
+        case 'B': 
+        case 'b': 
+            return"1011"; 
+           
+        case 'C': 
+        case 'c': 
+          return "1100"; 
+         
+        case 'D': 
+        case 'd': 
+           return "1101"; 
+           
+        case 'E': 
+        case 'e': 
+            return "1110"; 
+           
+        case 'F': 
+        case 'f': 
+          return "1111"; 
+            
+        default: 
+           return "\nInvalid hexadecimal digit " ;
+                 
+       
+    } 
+    }
+    public static String hexa1bytetoBinary(char a , char b){
+    return hexatoBinary(a)+hexatoBinary(b);
+    
+    
+    }
+      public static String BinaryToHexa(String binary){
+    int[] hex = new int[2];
+  int i = 1, j = 0, rem, dec = 0, bin;
+
+
+  bin = Integer.parseInt(binary);
+  while (bin > 0) {
+   rem = bin % 2;
+   dec = dec + rem * i;
+   i = i * 2;
+   bin = bin / 10;
+  }
+   i = 0;
+  while (dec != 0) {
+   hex[i] = dec % 16;
+   dec = dec / 16;
+   i++;
+  }
+  String hexatext="";
+    for(int q=1;q>=0;q--){
+    switch(hex[q]){
+        case  10 :
+        hexatext+="A";
+        break;
+        case 11 :
+         hexatext+="B";
+         case  12 :
+        hexatext+="C";
+        break;
+        case  13 :
+        hexatext+="D";
+        break;
+        case  14 :
+        hexatext+="E";
+        break;
+        case  15 :
+        hexatext+="F";
+        break;
+        default:
+              hexatext+=hex[q];
+
+    
+    }
+  
+    }
+    return hexatext;
+    }
+      public static String BinarytoPoly(String bn){
+      String ret="";
+      for(int i=0;i<bn.length();i++){
+          if(i==bn.length()-1 && bn.charAt(i)=='1')
+              ret+="1+";
+     else if(bn.charAt(i)!='0')
+          ret+="x^"+(bn.length()-1-i)+"+";
+          
+      
+      }
+     return ret.substring(0, ret.length() - 1);
+      
+      }
+      public static String polytoBinary(String poly){
+        poly=  poly.replace('X', 'x');
+      char[]b={'0','0','0','0','0','0','0','0'};
+      if(poly.contains("x^7"))
+          b[0]='1';
+       if(poly.contains("x^6"))
+          b[1]='1';
+        if(poly.contains("x^5"))
+          b[2]='1';
+         if(poly.contains("x^4"))
+          b[3]='1';
+           if(poly.contains("x^3"))
+          b[4]='1';
+            if(poly.contains("x^2"))
+          b[5]='1';
+             if(poly.contains("x^1"))
+          b[6]='1';
+              if(poly.contains("x"))
+          b[6]='1';
+               if(poly.contains("+1"))
+          b[7]='1';
+             
+      
+      return String.valueOf(b);
+      
+      
+      }
+      public static String getSubbytefrombinary(String bin){
+     
+         String c="01100011";
+         char[] b=new char[8];
+         for(int i=7;i>=0;i--){
+             int  x= invsermodulo(i-4);
+             int  y= invsermodulo(i-5);
+             int  z= invsermodulo(i-6);
+             int  f= invsermodulo(i-7);
+             
+            
+                 
+            
+         b[i]=xor( xor (xor( xor (xor(bin.charAt(i) , bin.charAt(x) ) , bin.charAt(y)  ) , bin.charAt(z)  ),bin.charAt(f)),c.charAt(i));
+         
+         }
+          System.out.println(String.copyValueOf(b));
+         return BinaryToHexa(String.copyValueOf(b));
+      
+      }
+      public static String SubstituteByteTransformation(char a , char b){
+         String bn=hexa1bytetoBinary(a, b);
+         String poly= BinarytoPoly(bn);
+       String polyinverse= findInverse(poly);
+       String bninverse=polytoBinary(polyinverse);
+       
+ 
+      return getSubbytefrombinary(bninverse);
+      
+          
+      }
+      public static int invsermodulo(int i){
+      
+      if(i<0)
+          return i+8;
+                  return i;
+      }
+      
+      
+      public static char xor(char a , char b){
+      if(a==b)
+          return '0';
+      return '1';
+      
+      }
+     
 }
--- src/aesproject/Polynomial.java
+++ src/aesproject/Polynomial.java
@@ -1,126 +1,137 @@
+package aesproject;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aesproject;
+
 
 /**
  *
  * @author HASAN
  */
-public class Polynomial {
-    private int deg[];
 
-	public Polynomial(int b, int a) {
-		deg = new int[b + 1];
-		if (a != 0)
-			deg[b] = 1;
+public class Polynomial {
+
+	int deg[];
+
+	public Polynomial(int a) {
+		deg = new int[9];
+		if (a != -1) {
+			deg[a] = 1;
+		}
 
 	}
 
 	public Polynomial(Polynomial p) {
-		this.deg = new int[p.deg.length];
-		for (int i = 0; i < p.deg.length; i++) {
+		this.deg = new int[p.deg.length-1];
+		for (int i = 0; i < this.deg.length; i++) {
 			this.deg[i] = p.deg[i];
 		}
 
 	}
 
 	public Polynomial plus(Polynomial b) {
-		Polynomial c = new Polynomial(Math.max(this.deg.length - 1, b.deg.length - 1), 0);
-		for (int i = 0; i < this.deg.length; i++)
+		Polynomial c = new Polynomial(-1);
+		for (int i = 0; i < this.deg.length; i++) {
 			c.deg[i] = (c.deg[i] + this.deg[i]) % 2;
-		for (int i = 0; i < b.deg.length; i++)
+		}
+		for (int i = 0; i < b.deg.length; i++) {
 			c.deg[i] = (c.deg[i] + b.deg[i]) % 2;
+		}
 		return c;
 	}
 
 	public Polynomial minus(Polynomial b) {
-		Polynomial c = new Polynomial(Math.max(this.deg.length - 1, b.deg.length - 1), 0);
-		for (int i = 0; i < this.deg.length; i++)
+		Polynomial c = new Polynomial(-1);
+		for (int i = 0; i < this.deg.length; i++) {
 			c.deg[i] = c.deg[i] + this.deg[i] % 2;
-		for (int i = 0; i < b.deg.length; i++)
+		}
+		for (int i = 0; i < b.deg.length; i++) {
 			c.deg[i] = Math.abs((c.deg[i] - b.deg[i])) % 2;
-		int j = 0;
-		for (int i = 0; i < c.deg.length; i++) {
-			if (c.deg[i] == 1)
-				j = i;
 		}
-		Polynomial temp = new Polynomial(j, 0);
-		for (int i = 0; i < temp.deg.length; i++) {
-			temp.deg[i] = c.deg[i];
-		}
-		return temp;
+
+		return c;
 	}
 
 	public Polynomial times(Polynomial b) {
-		Polynomial c = new Polynomial(this.deg.length - 1 + b.deg.length - 1, 0);
+		Polynomial c = new Polynomial(-1);
 		for (int i = 0; i < this.deg.length; i++) {
 			for (int j = 0; j < b.deg.length; j++) {
-				if (this.deg[i] != 0 && b.deg[j] != 0)
+				if (this.deg[i] != 0 && b.deg[j] != 0) {
 					c.deg[i + j] = (c.deg[i + j] + 1) % 2;
+				}
 			}
 		}
 		return c;
 	}
 
-	public Polynomial[] divides(Polynomial b) {
-		Polynomial c = new Polynomial(this);
-		Polynomial q = new Polynomial(0, 0);
-		while (c.deg.length >= b.deg.length) {
-			Polynomial extra = new Polynomial(c.deg.length - b.deg.length, 1);
+	public Polynomial[] divides(Polynomial value) {
+		Polynomial f = this;
+		Polynomial q = new Polynomial(-1);
+		while (f.IndexOfone() >= value.IndexOfone()) {
+			Polynomial extra = new Polynomial(f.IndexOfone() - value.IndexOfone());
 			q = q.plus(extra);
-			c = c.minus(b.times(extra));
-			
+			f = f.minus(value.times(extra));
+		
 		}
-		return new Polynomial[] { q, c };
+		return new Polynomial[] { q, f };
 	}
 
 	public boolean isOne() {
 		boolean one = false;
-		if (this.deg[0] == 1)
+		if (this.deg[0] == 1) {
 			one = true;
+		}
 		boolean other = false;
 		for (int i = 1; i < this.deg.length; i++) {
-			if (this.deg[i] != 0)
+			if (this.deg[i] != 0) {
 				other = true;
+			}
 
 		}
-		if (one && !other)
+		if (one && !other) {
 			return true;
-		else
+		} else {
 			return false;
+		}
 	}
 
+        @Override
 	public String toString() {
 		String s = "";
 		for (int i = deg.length - 1; i >= 0; i--) {
 			if (deg[i] == 1) {
-				if (i == 1 && this.deg[i] == 1)
-					s += "X+";
-				else if (i == 0 && this.deg[i] == 1)
+				if (i == 1 && this.deg[i] == 1) {
+					s += "x+";
+				} else if (i == 0 && this.deg[i] == 1) {
 					s += "1+";
-				else
-					s += "X^" + i + "+";
+				} else {
+					s += "x^" + i + "+";
+				}
 			}
 		}
-		if (s.length() == 0)
+		if (s.length() == 0) {
 			return "0";
+		}
 		return s.substring(0, s.length() - 1);
 	}
 
 	public static Polynomial Add(String s) {
-		Polynomial temp = new Polynomial(0, 0);
+		Polynomial temp = new Polynomial(-1);
 		String arr[] = s.split("\\+");
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i].equals("1"))
-				temp = temp.plus(new Polynomial(0, 1));
-			else if (arr[i].equals("x")) {
-				temp = temp.plus(new Polynomial(1, 1));
+			if (arr[i].equals("0") || arr[i].equals("00"))
+				temp = temp.plus(new Polynomial(-1));
+			else if (arr[i].equals("1")) {
+				temp = temp.plus(new Polynomial(0));
+			} else if (arr[i].equals("x")) {
+				temp = temp.plus(new Polynomial(1));
 
-			} else
-				temp = temp.plus(new Polynomial(Integer.parseInt(arr[i].charAt(2) + ""), 1));
+			} else {
+				temp = temp.plus(new Polynomial(Integer.parseInt(arr[i].charAt(2) + "")));
+			}
 
 		}
 
@@ -129,35 +140,50 @@ public class Polynomial {
 
 	public boolean isZero() {
 		for (int j = 0; j < this.deg.length; j++) {
-			if (this.deg[j] != 0)
+			if (this.deg[j] != 0) {
 				return false;
+			}
 		}
 
 		return true;
 	}
-        	public static String findInverse(String q, String s , Polynomial A1 , Polynomial A2 , Polynomial B1 , Polynomial B2 ) {
-                    
-                    if(B2.isOne() || B2.isZero()){
-                    if (B2.isOne())
-			return B1.toString();
-                    return "no inverse for the input equation";
-                    } 
-                    
-                    
-      
-                    Polynomial arr[] = A2.divides(B2);
-			Polynomial temp = new Polynomial(B2);
-			B2 = new Polynomial(arr[1]);
-			A2 = new Polynomial(temp);
-			temp = new Polynomial(B1);
-			B1 = new Polynomial(A1.minus(B1.times(arr[0])));
-			A1 = new Polynomial(temp);
-                 return findInverse(q,s,A1,A2,B1,B2);
-                    
-                    
-                }
 
+	
         
         
-    
+        
+        public static Polynomial findpolyInverse(Polynomial A1 , Polynomial A2 , Polynomial B1 ,Polynomial B2 ) {
+		
+	if(B2.isOne() || B2.isZero()){
+        
+        if (B2.isOne()) {
+			return B1;
+		} else {
+			return new Polynomial(-1);
+		}
+        }
+            
+
+			Polynomial arr[] = A2.divides(B2);
+			Polynomial temp = B2;
+			B2 = arr[1];
+			A2 = temp;
+			temp = B1;
+			B1 = A1.minus(B1.times(arr[0]));
+			A1 = temp;
+
+		return findpolyInverse(A1,A2,B1,B2);
+		
+	}
+
+	
+	public int IndexOfone() {
+		for (int i = deg.length-1; i>=0; i--) {
+			if (deg[i] == 1) {
+				return i;}
+		}
+
+		return -1;
+	}
 }
+
