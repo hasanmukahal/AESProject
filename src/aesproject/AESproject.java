a0e13eb4666ce6b022bd307ad0e977d0b814d7d1
hasanmukahal <hasan000215@hotmail.com>
2019-11-26 15:40:44 +0200

    Add files via upload

--- src/aesproject/AESproject.java
+++ src/aesproject/AESproject.java
@@ -29,14 +29,26 @@ public class AESproject {
         // System.out.println(SubstituteByteTransformation(Polynomial.findInverse(b,m,A1,A2,B1,B2)));
         //    System.out.println(hexa1bytetoBinary('8', 'A'));
         // System.out.println(getSubbytefrombinary(hexa1bytetoBinary('8', 'A')));
-        // System.out.println(SubstituteByteTransformation('9','5'));
+        //System.out.println(SubstituteByteTransformation('0', '0'));
         //   System.out.println(multwoOneByteHexa("8c" , "02"));
         String[][] c = {{"87", "f2", "4d", "97"},
         {"6e", "4c", "90", "ec"},
         {"46", "e7", "4a", "c3"},
         {"a6", "8c", "d8", "95"}
         };
-        printcontentoftwodimetionalarray(mixcol(c));
+        //printcontentoftwodimetionalarray(mixcol(c));
+
+        String[][] k = {{"2b", "28", "ab", "09"},
+        {"7e", "ae", "f7", "cf"},
+        {"15", "d2", "15", "4f"},
+        {"16", "a6", "88", "3c"}
+        };
+        String[][] k2 = {{"ef", "a8", "b6", "db"},
+        {"44", "52", "71", "0b"},
+        {"a5", "5b", "25", "ad"},
+        {"41", "7f", "3b", "00"}
+        };
+        printcontentoftwodimetionalarray(KeyExpantion(k2, 5));
 
     }
 
@@ -195,15 +207,21 @@ public class AESproject {
 
     public static String BinarytoPoly(String bn) {
         String ret = "";
+        boolean flag = true;
         for (int i = 0; i < bn.length(); i++) {
             if (i == bn.length() - 1 && bn.charAt(i) == '1') {
                 ret += "1+";
+                flag = false;
             } else if (bn.charAt(i) != '0') {
                 ret += "x^" + (bn.length() - 1 - i) + "+";
+                flag = false;
             }
 
         }
-        return ret.substring(0, ret.length() - 1);
+        if (!flag) {
+            return ret.substring(0, ret.length() - 1);
+        }
+        return ret;
 
     }
 
@@ -249,7 +267,6 @@ public class AESproject {
     }
 
     public static String getSubbytefrombinary(String bin) {
-
         String c = "01100011";
         char[] b = new char[8];
         for (int i = 7; i >= 0; i--) {
@@ -261,12 +278,14 @@ public class AESproject {
             b[i] = xor(xor(xor(xor(xor(bin.charAt(i), bin.charAt(x)), bin.charAt(y)), bin.charAt(z)), bin.charAt(f)), c.charAt(i));
 
         }
-        System.out.println(String.copyValueOf(b));
         return BinaryToHexa(String.copyValueOf(b));
 
     }
 
     public static String SubstituteByteTransformation(char a, char b) {
+        if (a == '0' && b == '0') {
+            return "63";
+        }
         String bn = hexa1bytetoBinary(a, b);
         String poly = BinarytoPoly(bn);
         String polyinverse = findInverse(poly);
@@ -330,6 +349,98 @@ public class AESproject {
             System.out.println();
         }
 
+    }
+
+    public static String[][] KeyExpantion(String[][] RoundKey, int roundnum) {
+        String hexab = getB(roundnum);
+        String[] w0 = getColumn(RoundKey, 0);
+        String[] w1 = getColumn(RoundKey, 1);
+        String[] w2 = getColumn(RoundKey, 2);
+        String[] w3 = getColumn(RoundKey, 3);
+
+        String[] w4 = xor1word(w0, gFunction(w3, hexab));
+        String[] w5 = xor1word(w4, w1);
+        String[] w6 = xor1word(w5, w2);
+        String[] w7 = xor1word(w6, w3);
+        String[][] Key = new String[4][4];
+        Key[0] = w4;
+        Key[1] = w5;
+        Key[2] = w6;
+        Key[3] = w7;
+        //Code to transpose a matrix  
+        String[][] transpose = new String[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                transpose[i][j] = Key[j][i];
+            }
+        }
+        return transpose;
+
+    }
+
+    public static String[] gFunction(String[] w0, String hexab) {
+        String[] shiftedw0 = new String[w0.length];
+        for (int i = 0; i < w0.length - 1; i++) {
+            shiftedw0[i] = w0[i + 1];
+        }
+        shiftedw0[w0.length - 1] = w0[0];
+        for (int i = 0; i < w0.length; i++) {
+            shiftedw0[i] = SubstituteByteTransformation(shiftedw0[i].charAt(0), shiftedw0[i].charAt(1));
+            if (i == 0) {
+                shiftedw0[0] = xorOneByte(shiftedw0[0], hexab);
+            }
+        }
+        return shiftedw0;
+
+    }
+
+    public static String getB(int roundnum) {
+
+        switch (roundnum) {
+            case 1:
+                return "01";
+            case 2:
+                return "02";
+            case 3:
+                return "04";
+            case 4:
+                return "08";
+            case 5:
+                return "10";
+            case 6:
+                return "20";
+            case 7:
+                return "40";
+            case 8:
+                return "80";
+            case 9:
+                return "1b";
+            case 10:
+                return "36";
+            default:
+                return "00";
+
+        }
+
+    }
+
+    public static String[] xor1word(String[] w1, String[] w2) {
+        String[] arr = new String[w1.length];
+        for (int i = 0; i < w1.length; i++) {
+            arr[i] = xorOneByte(w1[i], w2[i]);
+
+        }
+
+        return arr;
+    }
+
+    public static String[] getColumn(String[][] array, int index) {
+        String[] column = new String[array[0].length];
+        for (int i = 0; i < column.length; i++) {
+            column[i] = array[i][index];
+        }
+        return column;
     }
 
 }
